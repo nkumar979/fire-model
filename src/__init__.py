0e70a3059bdedd7fdb4e678d19418e41e0a19675
nirusenthilkumar <nkumar11393@gmail.com>
2024-03-09 15:55:03 -0800

    cleaning up init and main function

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 # fire_prediction_package/__init__.py
+# import os
+# import sys
+# path = os.getcwd()
+# sys.path.append(path)
 
-from fire_prediction_model.predict_fire import xgb_model
+from .fire_prediction_model import predict_fire
 
 __all__ = ['predict_fire']
---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+
+
+from fire_prediction_model import predict_fire
+
+def main():
+    predict_fire.xgb_model()
+
+if __name__ == "__main__":
+    main()
+
